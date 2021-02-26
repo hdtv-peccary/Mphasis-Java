9e9e50fe8ff6ad3b14ab3fbdf8593ef8751ef147
PriyadarshiniP <priya.palani248@gmail.com>
2021-02-26 21:09:12 +0530

    FixedThreadPool

---
+++ Threads/ExecutorExample.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+class RunClass implements Runnable{
+
+    @Override
+    public void run() {
+
+
+            System.out.println(Thread.currentThread());
+
+    }
+}
+public class ExecutorExample {
+    public static void main(String[] args){
+        int coreCount = Runtime.getRuntime().availableProcessors();
+        System.out.println(coreCount);
+       ExecutorService service = Executors.newFixedThreadPool(coreCount);
+
+
+ for(int counter = 0; counter<10;counter++){
+     service.execute(new RunClass());
+ }
+
+ service.shutdown();
+    }
+}
