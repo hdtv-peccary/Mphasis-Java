86615a675b1474f7b02c2282be418513c8a83b90
PriyadarshiniP <priya.palani248@gmail.com>
2021-02-26 21:31:18 +0530

    CachedThreadPool

---
+++ Threads/CachedExample.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+    class RunClas implements Runnable{
+
+        @Override
+        public void run() {
+
+
+            System.out.println(Thread.currentThread());
+
+        }
+    }
+    public class CachedExample {
+        public static void main(String[] args){
+            int coreCount = Runtime.getRuntime().availableProcessors();
+            System.out.println(coreCount);
+            ExecutorService service = Executors.newCachedThreadPool();
+            //if corecount is hardcoded , it vl create the threads
+            // and scheduler decides which one to execute
+
+            for(int counter = 0; counter<10;counter++){
+                service.execute(new RunClas());
+            }
+
+            service.shutdown();
+        }
+
+}
