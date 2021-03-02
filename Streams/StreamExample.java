a012df7019ae7fde3d032c558128ac1e14230850
PriyadarshiniP <priya.palani248@gmail.com>
2021-03-02 13:57:30 +0530

    Streams & list update

--- Streams/StreamExample.java
+++ Streams/StreamExample.java
@@ -1,7 +1,11 @@
 package com.mph;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamExample {
     public static void main(String[] args) {
@@ -17,19 +21,54 @@ public class StreamExample {
 //                .map(e-> e * 2.0)
 //                .forEach(System.out::println);
 
-        numbers.stream()
-                .map(String::valueOf)
-                .forEach(System.out::println);
+//        numbers.stream()
+//                .map(String::valueOf)
+//                .forEach(System.out::println);
+//
+//        System.out.println(numbers.stream()
+//                .reduce(0,(total,e)->Integer.sum(total,e)));
+//        System.out.println(numbers.stream()
+//                .reduce(0,Integer::sum));
+//
+//         numbers.stream()
+//                 .map(e->String.valueOf(e))
+//                 .reduce("",(concatString,str)->concatString.concat(str));
+//
+//        System.out.println(numbers.stream()
+//                            .map(e -> String.valueOf(e))
+//                             .reduce("",String::concat));
+//        System.out.println(numbers.stream()
+//                .mapToInt(e -> Integer.valueOf(e))
+//                .sum());
+//
+//        numbers.stream()
+//                .filter(e-> e%2 ==0)
+//                .map(e-> e*2)
+//                .forEach(System.out::println);
+//
+//        System.out.println(numbers.stream()
+//                .filter(e-> e%2 ==0)
+//                .map(e-> e*2)
+//                .reduce(0,Integer::sum));
+//
+//        System.out.println(numbers.stream()
+//        .filter(e->e%2==0)
+//        .reduce(0,(total,e)->Integer.sum(total,e)));
 
-        System.out.println(numbers.stream()
-                .reduce(0,(total,e)->Integer.sum(total,e)));
+            List<Integer> dNumbers  = Arrays.asList(1,2,3,4,5,1,2,3,4,5);
+            List<Integer> DoubleEven = dNumbers.stream()
+                    .filter(e->e%2==0)
+                    .map(e->e*2)
+                    .collect(Collectors.toList());
+        System.out.println(DoubleEven);
 
-         numbers.stream()
-                 .map(e->String.valueOf(e))
-                 .reduce("",(concatString,str)->concatString.concat(str));
 
-        System.out.println(numbers.stream()
-                            .map(e -> String.valueOf(e))
-                             .reduce("",String::concat));
+                   // .forEach(e->DoubleEven.add(e)); //shared and mutated
+
+
     }
+
+
 }
+
+
